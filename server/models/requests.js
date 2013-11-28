cdc5b1897af0ec76a71999cc2ff5527030d070f8
jacquarg <guillaume.jacquart@hoodbrains.com>
2013-11-28 22:11:30 +0100

    CRA Step 1 & 2. Requests, and server done.

--- server/models/requests.js
+++ server/models/requests.js
@@ -37,7 +37,52 @@ module.exports = {
     person: {
         all: americano.defaultRequests.all
         
-    }
+    },
 
+    phonecommunicationlog: {
+        byTimestamp: byTimestamp,
+
+        dayAbstract: {
+            map: function(doc) {
+                emit(doc.timestamp.substring(0,10), doc);
+            },
+
+            reduce: function(key, values, rereduce) {
+                var sums = {
+                       calls: 0,
+                       sms : 0,
+                       data : 0
+                    }
+                if (!rereduce) {
+                    for (var idx=0; idx<values.length; idx++) {
+                        v = values[idx];
+                        
+                        if (v.type == 'VOICE') {
+                            sums.calls += 1;
+
+                        } else if (v.type == 'SMS-MSC') {
+                            sums.sms += 1;
+                        } else if (v.type == 'DATA') {
+                            sums.data += v.chipCount;
+                        }
+
+                        // else :SMS-C ? , MMS ? SERVICE ?
+                    }
+
+                } else { // rereduce
+                    for (var j=0; j<values.length; j++) {
+                        v = values[j];
+                        sums.calls += v.calls;
+                        sums.sms += v.sms;
+                        sums.data += v.data;
+
+                    }
+                }
+                
+                return sums;
+            }
+
+        }
+    }
 
 };
